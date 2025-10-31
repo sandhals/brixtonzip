1ddcebf9983270689b73db20305f34284c896385
Brixton Sandhals <brixtonsandhals@Brixtons-MacBook-Pro.local>
2025-10-31 11:19:22 +0900

    updated colophon

--- pages/colophon.tsx
+++ pages/colophon.tsx
@@ -7,7 +7,8 @@ export default function LanguagePage() {
             <HeaderImage src="/images/laptop.png" alt="a cafe spread with my laptop" />
       <div className="box">
         <h1>Colophon</h1>
-        <p>The main visual design inspiration for my site comes from receipt printer photobooths that were becoming popular in Seoul around the time that I started building it.</p>
+        <p>The main visual inspiration for my site comes from the <a href="https://briefcompany.net">receipt camera</a> photobooths that were becoming popular in Korean cafes around the time the time I registered this domain.</p>
+        <p>I originally wrote this site in pure HTML, but eventually it sort of became my experimentation ground for learning TSX. Somehow, it felt like a betrayal of my indie web roots, but switching to from static to dynamic allowed me use things pre-built layouts and external APIs, such as the <a href="http://curius.app">Curius</a> reading list I have displayed on my homepage (an idea I stole from <a href="http://benneo.xyz"></a>).</p>
               
 
         <br />
